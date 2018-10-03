98636b130041bc483ebfeb2d99f5074dacf8be2b
Erik Lonroth <erik.lonroth@scania.com>
2018-10-03 14:35:40 +0200

    property not callable

--- reactive/layer_munge.py
+++ reactive/layer_munge.py
@@ -44,7 +44,7 @@ def consume_munge_key(munge_consumer):
     '''consume a munge key if a relation to a provider has been made
     via a consumer interface regardless of whether it has been generated
     or not. Store it in leader settings to propagate to other units.'''
-    munge_key = munge_consumer.munge_key()
+    munge_key = munge_consumer.munge_key
     # do not do anything unless there is actually a key available
     # otherwise, keep using whatever was there before
     if munge_key:
