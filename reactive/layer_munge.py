3e1da377cd1f9002218dc9064e04aeb395c03530
Erik Lönroth <erik.lonroth@gmail.com>
2018-10-18 01:59:53 +0200

    Merge to recover lost changes.... (#1)
    * Added so that charm config changes will be sent to the consumers
    
    * Change of munge key in provider now gets published to consumer

--- reactive/layer_munge.py
+++ reactive/layer_munge.py
@@ -9,8 +9,10 @@ import charmhelpers.core.host as host
 MUNGE_PACKAGE = 'munge'
 
 
-flags.register_trigger(when='config.changed.munge_key',
-                       clear_flag='munge.configured')
+# This trigger does not work. Only config.changed works.
+# Moved to config_changed() below
+#flags.register_trigger(when='config.changed.munge_key',
+#                       clear_flag='munge.configured')
 flags.register_trigger(when='leadership.changed.munge_key',
                        clear_flag='munge.configured')
 flags.register_trigger(when='leadership.changed.munge_key',
@@ -18,8 +20,9 @@ flags.register_trigger(when='leadership.changed.munge_key',
 
 
 @reactive.when_not('munge.installed')
-def install_slurm():
+def install_munge():
     hookenv.status_set('maintenance', 'installing munge package')
+    hookenv.log('install_munge(): installing munge package')
 
     packages = [MUNGE_PACKAGE]
     apt_install(packages)
@@ -34,8 +37,29 @@ def obtain_munge_key(*args):
     munge_key = hookenv.config().get('munge_key')
     # Generate a munge key if it has not been provided via charm config
     if not munge_key:
+        hookenv.log('obtain_munge_key(): No key in charm config, generating new key')
         munge_key = host.pwgen(length=4096)
+    else:
+        hookenv.log('obtain_munge_key(): Using key from charm config')
     leadership.leader_set(munge_key=munge_key)
+
+@reactive.when('leadership.is_leader')
+@reactive.when('config.changed')
+def config_changed():
+    hookenv.log('config_changed(): leader detected charm config change')
+    charmconf = hookenv.config()
+    # We are only interested if the munge_key has changed
+    if charmconf.changed('munge_key'):
+        hookenv.log('config:changed(): munge key has changed')
+        prev_key = charmconf.previous('munge_key')
+        munge_key = charmconf['munge_key']
+        hookenv.log('config:changed(): previous key: %s' % prev_key)
+        hookenv.log('config:changed(): new key: %s' % munge_key)
+        leadership.leader_set(munge_key=munge_key)
+        # clear munge.configured and munge.exposed
+        flags.clear_flag('munge.configured')
+        flags.clear_flag('munge.exposed')
+        flags.set_flag('munge.changed_key_file')
 
 
 @reactive.when('endpoint.munge-consumer.munge_key_updated')
@@ -58,13 +82,27 @@ def consume_munge_key(munge_consumer):
 def configure_munge_key():
     munge_key = leadership.leader_get('munge_key')
     munge.render_munge_key(context={'munge_key': munge_key})
+    hookenv.log('configure_munge_key(): leadership detected new munge key, rendered new file')
     # set a flag confirming that munge key is rendered
     flags.set_flag('munge.configured')
 
 
-@reactive.when_file_changed(munge.MUNGE_KEY_PATH)
-def restart_on_munge_change():
+@reactive.when('munge.installed')
+@reactive.when('munge.configured')
+@reactive.when('munge.changed_key_file')
+def restart_on_munge_change2():
+    hookenv.log('restart_on_munge_change2(): file %s modified, restarting due to flag' % munge.MUNGE_KEY_PATH)
     host.service_restart(munge.MUNGE_SERVICE)
+    flags.clear_flag('munge.changed_key_file')
+
+# This only seems to work when we change through leadership, not charm config change.
+# Also, deprecated? https://github.com/juju-solutions/charms.reactive/issues/44
+#@reactive.when_file_changed(munge.MUNGE_KEY_PATH)
+#def restart_on_munge_change():
+#    ''' The when_file_changed gets triggered on leader-set munge_key=xxx but not
+#    when key on disk is written due to a charm config change. Needs an extra flag. '''
+#    hookenv.log('restart_on_munge_change(): file %s was modified, restarting munge' % munge.MUNGE_KEY_PATH)
+#    host.service_restart(munge.MUNGE_SERVICE)
 
 
 @reactive.when('endpoint.munge-provider.joined')
@@ -72,8 +110,10 @@ def restart_on_munge_change():
 @reactive.when('leadership.set.munge_key')
 @reactive.when('munge.configured')
 @reactive.when_not('munge.exposed')
-def provide_munge_key(munge_provider):
+def provide_munge_key_to_interface(munge_provider):
     '''Provide munge key if any consumers are related and if '''
     munge_key = leadership.leader_get('munge_key')
+    hookenv.log('provide_munge_key_to_interface(): exposing munge key: %s' % munge_key)
     munge_provider.expose_munge_key(munge_key)
+    munge_provider.provide_munge_key()
     flags.set_flag('munge.exposed')
